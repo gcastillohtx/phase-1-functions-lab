3c113e456d821c7e727c879a8e582b11628a8d77
Gil <gcastillohtx@gmail.com>
2022-01-10 12:33:55 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+  if (someValue < 42) {
+    return 42 - someValue;
+  } else {
+    return someValue - 42;
+  }
+}
+//////////////
+function distanceFromHqInFeet(someValue) {
+  let distance = distanceFromHqInBlocks(someValue);
+  return 264 * distance;
+}
+/////////////
+function distanceTravelledInFeet(start, destination) {
+ if (destination >= start) {
+   return (destination-start) *264
+ } else {
+   return (start - destination) *264
+ }
+}
+
+function calculatesFarePrice (start, destination) {
+  const distance = distanceTravelledInFeet(start, destination)
+  if (distance < 400) {
+    return 0;
+  } else if (distance > 400 && distance < 2000) {
+    return (distance - 400) * 0.02;
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+  }
+
+
+
+
+
+  
+
+
+
